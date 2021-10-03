c1a3414989c1378d15fe6f169ee577dcff7bd6b4
Mel <meljohnpualon@gmail.com>
2021-10-04 02:54:14 +0800

    test commit - vs code ui

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import TodoItem from './TodoItem'
-
+// test push vscode
 export default class TodoList extends Component {
     render() {
         return (
