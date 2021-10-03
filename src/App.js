33db2cb7d416cfd35f5c995774e409908e966c3a
Mel <meljohnpualon@gmail.com>
2021-10-04 03:12:37 +0800

    added methods and state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
-import uuid from 'uuid'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 export default class App extends Component {
+  state={
+    items: [{id: 1, title: 'Code now'}, {id: 2, title: 'Code later'}],
+    id: uuidv4(),
+    item: '',
+    edititem: false
+  }
+
+  handleChange = e => {
+    console.log('typed change')
+  }
+
+  handleSubmit = e => {
+    console.log('submitted')
+  }
+
+  clearList = () => {
+    console.log('Clear list')
+  }
+
+  handleEdit = e => {
+    console.log(`handle edit ${id}`)
+  }
+
+  handleDelete = e => {
+    console.log(`deleted item ${id}`)
+  }
+
+
   render() {
+    // console.log(this.state.id)
+
     return (
       <div className="container">
         <div className="row">
