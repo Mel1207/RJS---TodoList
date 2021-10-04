84d2d44d3359b36153ff92bf70076c8e3a9e1a5a
Mel <meljohnpualon@gmail.com>
2021-10-04 21:49:18 +0800

    added todo list component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,15 @@ import TodoItem from './TodoItem'
 // test push vscode
 export default class TodoList extends Component {
     render() {
+        const {items, clearList, handleDelete, handleEdit} = this.props
+
         return (
             <div>
-                <h2>Todo List</h2>
-                <TodoItem />
+                <ul className="list-group my-5">
+                    <h3 className="text-capitalize text-center">Todo List</h3>
+                    {items.map(i => {return (<TodoItem key={i.id} title={i.title} handleDelete={() => handleDelete(i.id)} handleEdit={() => handleEdit(i.id)}></TodoItem>)})}
+                </ul>
+                <button type="button" className="btn btn-danger col-12 text-white mt-5" onClick={clearList}>Clear List</button>
             </div>
         )
     }
