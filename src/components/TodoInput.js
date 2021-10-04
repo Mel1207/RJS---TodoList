15e7cfff400ec73a792b3af404b7d84a299b6191
Mel <meljohnpualon@gmail.com>
2021-10-04 20:05:29 +0800

    added input and submit component

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-import { ArrowRight } from 'react-bootstrap-icons'
+import { BsFillBookFill, BsPlusLg } from 'react-icons/bs'
 
 export default class TodoInput extends Component {
     render() {
+        const {item, handleChange, handleSubmit, editItem} = this.props
+
         return (
-            <div>
-                <h1>this is todo input</h1>
+            <div className="card card-body mt-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i>
+                                    <BsFillBookFill/>
+                                </i>
+                            </div>
+                        </div>
+                        <input type="text" className="form-control text-capitalize" placeholder="add to do" value={item} onChange={handleChange}/>
+                    </div>
+                    <button type="submit" className="btn col-12 btn-primary mt-3">
+                        <i className="text-red">
+                            <BsPlusLg />
+                        </i>
+                        Add item</button>
+                </form>
+                
             </div>
         )
     }
